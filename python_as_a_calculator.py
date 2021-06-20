d6f620d384aa77ce5b90168856acdb21063df873
Adithya Visnu <adithyavisnu@gmail.com>
2021-06-20 21:14:03 +0700

    course first python math

---
+++ python_as_a_calculator.py
@@ -0,0 +1,9 @@
+# Suppose you have $100, which you can invest
+# with a 10% return each year. After one year, 
+# it's  dollars, and after two years it's . 
+# Add code to calculate 
+# how much money you end up with after 7 years, 
+# and print the result.
+
+
+print(100 * ( 1.1 ** 7 ))
