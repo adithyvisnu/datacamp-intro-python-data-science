a2b5095127a42bc7d687a6c52549cf9a4c6adcea
Adithya Visnu <adithyavisnu@gmail.com>
2021-06-20 22:00:41 +0700

    update variables and types

---
+++ variables_and_types.py
@@ -0,0 +1,17 @@
+# Calculate BMI
+# Body Mass Index
+
+height = 1.72
+weight = 57.3
+bmi = weight / height ** 2
+
+print(bmi, type(bmi))
+
+name = 'Adithya Visnu'
+print(name, type(name))
+
+isItTrue = True
+print(isItTrue, type(isItTrue))
+
+numberOfQueue = 5
+print(numberOfQueue, type(numberOfQueue))
